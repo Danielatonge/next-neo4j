39f37ea65057060d86067655f2ef2dd84d662204
Daniel Atonge <elamboatonge@gmail.com>
2022-02-16 05:50:16 +0300

    corrects micro issue

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -2,7 +2,6 @@ import { gql, ApolloServer } from "apollo-server-micro"
 import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core"
 import { Neo4jGraphQL } from "@neo4j/graphql"
 import neo4j from "neo4j-driver"
-import "ts-tiny-invariant"
 
 const typeDefs = gql`
 scalar Point
